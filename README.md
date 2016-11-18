1af87d65cb8d5727bc279a090d784dc988b0e1ef
Thomas Bullock <motbollox@gmail.com>
2016-11-18 14:44:51 +1100

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Dynamic Cheat Sheet for Front End Developers
+
+This a co-corricular activity from Free Code Camps forum. 'Frontend Web Development Challenge - November 16 to 23'
+
+Aim is to create a dynamic searchable cheatsheet in a Single page application.
+
+Cheat sheets for Git, Terminal, Bourbon, Gulp and perhaps more.
+
+Technologies to be used
+
+Knockout JS, JQuery & Bourbon/Neat 
+
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+		sass = require('gulp-sass'),
+		maps = require('gulp-sourcemaps'),
+		 del = require('del');
+
+gulp.task("concatScripts", function(){
+		return gulp.src([
+			'src/javascript/knockout-3.4.0.js', 
+			'node_modules/jquery/dist/jquery.min.js',
+			'src/javascript/main.js'])
+		.pipe(maps.init())
+		.pipe(concat('app.js'))
+		.pipe(maps.write('./'))		
+		.pipe(gulp.dest('js'));
+});
+
+
+gulp.task('compileSass', function(){
+		return gulp.src("src/sass/styles.scss")
+		.pipe(maps.init())
+		.pipe(sass())
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest('css'));
+})
+
+gulp.task('watchFiles', function() {
+	gulp.watch('src/sass/**/*.scss', ['compileSass']);
+	gulp.watch('src/javascript/main.js', ['concatScripts'])
+})
